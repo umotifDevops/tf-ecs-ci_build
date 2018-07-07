ebef83a27f42e5f76f35ba24589071681ed871e6
umotifDevops <conect2mo@yahoo.co.uk>
2018-07-07 19:29:17 +0100

    Terraform untainted

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "docker_image" "image_id" {
   name = "ghost:latest"
 
 }
+
+#start the container
+resource "docker_container" "container_id"{
+  name = "blog"
+  image = "${docker_image.image_id.latest}"
+  ports {
+       internal = "2343"
+       external = "80"
+}
+}
+
