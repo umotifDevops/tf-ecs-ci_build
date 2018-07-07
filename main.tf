85325e54d2911f6dc19f1675acb0418b7f338711
umotifdevops@devops <conect2mo@yahoo.co.uk>
2018-07-07 23:40:10 +0100

    Variables

--- main.tf
+++ main.tf
@@ -1,18 +1,45 @@
 #download the latest Ghost image
 
+variable "image" {
+  description = "image for container"
+  default = "ghost"
+}
+
+variable "container_name" {
+  description = "Name of container"
+  default = "Blog"
+}
+
+variable "int_port" {
+  description = "Internal Port for container"
+  default = "2343"
+}
+
+variable "ext_port" {
+  description = "External Port for container"
+  default = "80"
+}
+ 
+
 resource "docker_image" "image_id" {
 
-  name = "ghost:latest"
+  name = "${var.image}"
 
 }
 
 #start the container
 resource "docker_container" "container_id"{
-  name = "blog"
+  name = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-       internal = "2343"
-       external = "80"
+       internal = "${var.int_port}"
+       external = "${var.ext_port}"
 }
+
 }
+
+output "Ip Address" {
+  value = "${docker_container.container_id.ip_address}"
+}
+
 
