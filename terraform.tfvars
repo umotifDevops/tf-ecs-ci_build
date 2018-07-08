929ca4c01bef5d1fb0e5b2d47066859ee17599e5
umotifdevops@devops <conect2mo@yahoo.co.uk>
2018-07-08 11:49:27 +0100

    Workspaces and Terraform.tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,19 @@
+image = {
+  dev = "ghost"
+  prod = "centos"
+}
+
+container_name = {
+  dev = "Gulam_Mohammed"
+  prod ="Khaja"
+}
+
+int_port = { 
+  dev = "2222"
+  prod= "2229"
+}
+
+ext_port = { 
+  dev = "8008"
+  prod = "8990"
+}
--- vars.tf
+++ vars.tf
@@ -4,35 +4,19 @@ variable "env" {
 variable "image" {
 description = "image for container"
   type = "map"
-  default = {
-     dev  =  "ghost"
-     prod = "centos"
-}
 }
 
 variable "container_name" {
   description = "Name of container"
   type = "map"
-  default = { 
-      dev = "Gulam_Mohammed"
-      prod = "Khaja"
-}
 }
 variable "int_port" {
   description = "Internal Port for container"
   type = "map"
-  default = { 
-       dev = "2353"
-       prod  = "2399"
-}
 }
 
 variable "ext_port" {
   description = "External Port for container"
   type ="map"  
-  default = { 
-      dev = "8090"
-      prod = "9090"
-}
 }
 
