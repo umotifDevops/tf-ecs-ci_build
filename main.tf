ffa50de47e0a8d8b41cdabdf90318c5f805925ac
umotifdevops@devops <conect2mo@yahoo.co.uk>
2018-07-08 11:33:58 +0100

    lookups

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "image" {
   source = "./image"
-  image = "${var.image}"
+  image = "${lookup(var.image, var.env)}"
 }
 
 module "container" {
   source = "./container"
   image = "${module.image.image_out}"
-  name = "${var.container_name}"
-  int_port = "${var.int_port}"
-  ext_port = "${var.ext_port}"
+  name = "${lookup(var.container_name, var.env)}"
+  int_port = "${lookup(var.int_port, var.env)}"
+  ext_port = "${lookup(var.ext_port, var.env)}"
 }
 
--- vars.tf
+++ vars.tf
@@ -1,20 +1,38 @@
+variable "env" {
+   description = "env : Dev or Prod"
+}
 variable "image" {
 description = "image for container"
-  default = "ghost"
+  type = "map"
+  default = {
+     dev  =  "ghost"
+     prod = "centos"
+}
 }
 
 variable "container_name" {
   description = "Name of container"
-  default = "Gulam_Mohammed"
+  type = "map"
+  default = { 
+      dev = "Gulam_Mohammed"
+      prod = "Khaja"
 }
-
+}
 variable "int_port" {
   description = "Internal Port for container"
-  default = "2353"
+  type = "map"
+  default = { 
+       dev = "2353"
+       prod  = "2399"
+}
 }
 
 variable "ext_port" {
   description = "External Port for container"
-  default = "8090"
+  type ="map"  
+  default = { 
+      dev = "8090"
+      prod = "9090"
+}
 }
 
